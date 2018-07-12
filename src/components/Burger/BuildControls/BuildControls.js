419d4609533491340c201310e623bcf1e040af11
Your Name <you@example.com>
2018-07-12 21:23:40 +0300

    toolbar

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl'
+
+const controls = [
+   { label: 'Salad', type: 'salad' },
+   { label: 'Bacon', type: 'bacon' },
+   { label: 'Meat', type: 'meat' },
+   { label: 'Cheese', type: 'cheese' },
+
+]
+const buildcontrols = (props) => (
+  <div className={classes.BuildControls}>
+      <p>Curret price: <strong>{props.price}</strong></p>
+      {controls.map(control => (
+       <BuildControl
+         key={control.label}
+         label={control.label}
+         added={() => props.ingredientAdded(control.type)}
+         removed={() => props.ingredientRemoved(control.type)}
+         disabled={props.disabled[control.type]}
+        />
+      ))}
+      <button
+        className={classes.OrderButton}
+        disabled={!props.purchasable}
+        onClick={props.ordered}
+        >ORDER NOW</button>
+  </div>
+);
+
+export default buildcontrols;
