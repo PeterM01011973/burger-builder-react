c380c2c616e02c8110a921b7efe7bafeed437930
Your Name <admin@localhost.com>
2018-10-12 09:00:59 +0300

    end auth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout';
@@ -12,23 +12,42 @@ import * as actions from './store/actions/index'
 class App extends Component {
 
 componentDidMount() {
-  this.props.onTryAutoSignUp();
+  this.props.onTryAutoSignUp(); 
 }
 
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/"  exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    )
+
+    if(this.props.isAuth) {
+      routes = (
+      <Switch>
+          <Route path="/checkout" component={Checkout} /> 
+          <Route path="/orders" component={Orders} /> 
+          <Route path="/logout" component={Logout} />
+          <Route path="/"  exact component={BurgerBuilder} /> 
+          <Redirect to='/' />
+      </Switch>
+      )
+    }
     return (
       <div>
           <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout} /> 
-              <Route path="/orders" component={Orders} /> 
-              <Route path="/"  exact component={BurgerBuilder} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/logout" component={Logout} />
-            </Switch>
+              { routes }
           </Layout> 
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
   }
 }
 
@@ -38,5 +57,5 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
  
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,7 +110,8 @@ class ContactData extends Component {
        const order = {
          ingredients: this.props.ings,
          price: this.props.price ,
-         orderData: fromData
+         orderData: fromData,
+         userId: this.props.userId
        }
        this.props.onBurgerBuilder(order, this.props.token)
     }
@@ -207,7 +208,8 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
